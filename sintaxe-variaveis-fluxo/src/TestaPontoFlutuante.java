bdf50de39ccb1d6b591ed6d021793075b184b335
al1neferreira <alinfst@gmail.com>
2023-01-04 22:02:06 -0300

    nova  classe para estudo de casting

--- sintaxe-variaveis-fluxo/src/TestaPontoFlutuante.java
+++ sintaxe-variaveis-fluxo/src/TestaPontoFlutuante.java
@@ -1,12 +1,13 @@
 public class TestaPontoFlutuante {
 
     public static void main(String[] args) {
-        double salario;
-        salario = 1250.70;
+        double salario = 1250.70;
 
         System.out.println("Meu salário é " + salario);
 
-        
+        double divisao = 5.0 / 2;
+        System.out.println(divisao);
+       
     }
     
 }
