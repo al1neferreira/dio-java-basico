120000db3c732b2dd699c60eba555bdba619cfc2
Aline Ferreira <alinfst@gmail.com>
2023-01-12 23:17:25 -0300

    Estudo Java - Atividade com condicionais

--- sintaxe-variaveis-fluxo/src/TestaCondicional2.java
+++ sintaxe-variaveis-fluxo/src/TestaCondicional2.java
@@ -1,6 +1,6 @@
 public class TestaCondicional2 {
-    
-    public static void main(String [] args) {
+
+    public static void main(String[] args) {
 
         System.out.println("Testando Condicionais");
 
