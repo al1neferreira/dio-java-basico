ceee637725f65bc657f0591c57eb9dde10ecef16
Aline Ferreira <alinfst@gmail.com>
2023-01-12 22:55:39 -0300

    Estudo Java - Escopo e inicialização de variável

--- sintaxe-variaveis-fluxo/src/TestaEscopo.java
+++ sintaxe-variaveis-fluxo/src/TestaEscopo.java
@@ -7,22 +7,25 @@ public class TestaEscopo {
 
         boolean acompanhado;
 
-        if (quantidadePessoas >=2) {
+        if (quantidadePessoas >= 2) {
             acompanhado = true;
         } else {
             acompanhado = false;
         }
 
-        System.out.println("valor de acompanhado = "+ acompanhado);
+        System.out.println("valor de acompanhado = " + acompanhado);
 
-        if (idade>=18 && acompanhado) {
+        if (idade >= 18 && acompanhado) {
 
             System.out.println("Seja bem-vindo");
         } else {
             System.out.println("Infelizmente você não pode entrar");
 
+
         }
     }
 
 }
+
+
 
