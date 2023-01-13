02a3ad9335010cefa86c2b3031d05205dcab7e49
Aline Ferreira <alinfst@gmail.com>
2023-01-12 22:02:22 -0300

    Estudo Java - Condicionais (if/else)

--- sintaxe-variaveis-fluxo/src/TestaCondicional.java
+++ sintaxe-variaveis-fluxo/src/TestaCondicional.java
@@ -5,16 +5,19 @@ public class TestaCondicional {
         System.out.println("Testando condicionais");
 
         int idade = 16;
+        int quantidadePessoas = 3;
+
         if (idade >= 18){
             System.out.println("você tem mais de 18 anos");
             System.out.println("seja bem-vindo");
             
         } else {
-            System.out.println("Infelizmente você não pode entrar");      
+            if (quantidadePessoas <=3){
+                System.out.println("Você não tem 18, mas pode entrar");
 
+            }else{
+                System.out.println("Infelizmente você não pode entrar, pois está acompanhado");
+            }
         }
-
-
-        
     }
 }
