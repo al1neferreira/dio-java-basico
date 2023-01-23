5f969b011be5de33ce03de038c7054fb24ec7fb9
Aline Ferreira <alinfst@gmail.com>
2023-01-22 21:20:09 -0300

    Estudo Java - Atividade com o uso do For

---
+++ sintaxe-variaveis-fluxo/src/MultiplosDeTresAteCem.java
@@ -0,0 +1,18 @@
+public class MultiplosDeTresAteCem {
+
+    public static void main(String[] args) {
+        for (int i = 3; i < 100; i += 3) {
+            System.out.println(i);
+        }
+
+        // opção 2//
+
+        for (int i = 1; i < 100; i++ ){
+            if (i % 3 == 0)    {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
+
